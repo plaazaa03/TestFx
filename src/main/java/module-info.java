75dfdc528a406ca410205c0b6456ef84e4cf294f
Raul <raulplaza208@gmail.com>
2024-03-13 09:19:39 +0100

    TestFx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module es.ieslosmontecillos.testfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires org.junit.jupiter.api;
+    requires org.testfx;
+    requires org.testfx.junit5;
+
+
+    opens es.ieslosmontecillos.testfx to javafx.fxml;
+    exports es.ieslosmontecillos.testfx;
+}
